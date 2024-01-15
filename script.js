cdd26a6bce3ac6b170a8937c4c9773f5057f9b25
krsolt2 <krsolt2@gmail.com>
2024-01-15 17:46:29 +0100

    final touches/added final score

--- script.js
+++ script.js
@@ -2,36 +2,36 @@ function getComputerChoice(choice) {
     choice = Math.floor(Math.random() * 3);
     
     if (choice == 0) {
-        console.log("Rock")
+        console.log("Computer choice: Rock")
         return choice = "rock"
     } else if (choice == 1) {
-        console.log("Paper")
+        console.log("Computer choice: Paper")
         return choice = "paper"
     } else {
-        console.log("Scissors")
+        console.log("Computer choice: Scissors")
         return choice = "scissors"
     }
-    console.log(choice)
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("Tie")
+        console.log("Tie / REPEAT ROUND")
+        return tie = 1
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("Win")
+        console.log("Win / +1 SCORE TO PLAYER")
         ++playerScore;
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("Win")
+        console.log("Win / +1 SCORE TO PLAYER")
         ++playerScore;
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("Win")
+        console.log("Win / +1 SCORE TO PLAYER")
         ++playerScore;
     }
     else {
-        console.log("Lose")
+        console.log("Lose / -1 SCORE TO PLAYER / +1 SCORE TO COMPUTER")
         --playerScore;
         ++computerScore;
     }
@@ -39,16 +39,27 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    for (let i = 0; i < 6; i++) {
-        const playerSelection = prompt("Enter your choice: ");
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Enter your choice: ").toLowerCase();
         const computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection)
-        if (i === 5) {
+        if (tie === 1) {
+            --i;
+            tie = 0;
+        }
+        if (i <= 5) {
             console.log("your score is: " + playerScore + " and computer score is: " + computerScore)
         }
     }
 }
 
+var tie = 0;
 var playerScore = 0;
 var computerScore = 0;
 game();
+
+if (playerScore > computerScore) {
+    console.log("You win! With a score of: " + playerScore + " vs " + computerScore + ".")
+} else {
+    console.log("You lost! With a score of: " + playerScore + " vs " + computerScore + ".")
+}
